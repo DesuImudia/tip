3461bd38650c9f6fff41be3daa0bad034ad52a70
DesuImudia <56054713+DesuImudia@users.noreply.github.com>
2020-08-11 23:01:57 -0400

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Desu Imudia on 8/11/20.
+//  Copyright © 2020 Desu Imudia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
